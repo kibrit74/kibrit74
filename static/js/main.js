b91cddd5994bacda4c80d69d1036c3cae89f4f24
kibrit74 <159079559+kibrit74@users.noreply.github.com>
2024-07-23 01:22:52 +0300

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+var userLoggedIn = USER_LOGGED_IN;
+
+function aramaYonlendir() {
+    if (userLoggedIn) {
+        window.location.href = "/profildata";
+    } else {
+        window.location.href = "/kayit";
+    }
+}
+=======
+var userLoggedIn = USER_LOGGED_IN;
+
+function aramaYonlendir() {
+    if (userLoggedIn) {
+        window.location.href = "/profildata";
+    } else {
+        window.location.href = "/kayit";
+    }
+}
+>>>>>>> e00910b5e17186036c7b59cf3b51aea5ec7bfc31
