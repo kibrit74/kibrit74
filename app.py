e9fd90438ea8b866a7a21b5261881e317d500acd
kibrit74 <159079559+kibrit74@users.noreply.github.com>
2024-07-24 00:21:19 +0300

    app.py

--- app.py
+++ app.py
@@ -1325,4 +1325,4 @@ def logout():
     session.pop('admin_username', None)
     return redirect(url_for('login'))
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), asyncio_server=True)
