3e182bca2091c338ce0597bf9de87dee64a4d09a
kibrit74 <159079559+kibrit74@users.noreply.github.com>
2024-07-23 19:39:56 +0300

    app.py

--- app.py
+++ app.py
@@ -1326,5 +1326,4 @@ def logout():
     return redirect(url_for('login'))
 
    if __name__ == '__main__':
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port)
+        app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
